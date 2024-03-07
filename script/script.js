8d35091bfe1e61be908ff801ab74da873f0b3cd6
Aligern <sasso.luca93@gmail.com>
2024-03-07 16:31:32 +0100

    second commit

--- script/script.js
+++ script/script.js
@@ -1,8 +1,39 @@
-let name;
-let age;
-let Kmeters;
-let priceKm = Kmeters * 0.21;
+let eluserName = document.getElementById('nome');
+console.log(eluserName);
+
+let elAge = document.getElementById('userage');
+console.log(elAge);
+
+let elKmeters = document.getElementById('kilometers');
+console.log(elKmeters);
+
+let priceKm = elKmeters * 0.21;
+console.log(priceKm);
+
 let under18 = 20 / 100;
+console.log(under18);
+
 let over65 = 40 / 100;
+console.log(over65);
+
+let elBtngen = document.getElementsByClassName('.btn.bg-primary');
+console.log(elBtngen);
+
+let elBtnnull = document.getElementsByClassName('.btn.bg-warning');
+console.log(elBtnnull);
+
+
+
+elBtngen.addEventListener('click', function(){
+    const nome = eluserName.value;
+    const eta = elAge.value;
+    const meters = elKmeters.value;
+    console.log(nome, eta, meters);
+} 
+)
+
+
+
+
 
 
