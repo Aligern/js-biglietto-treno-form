598b2e445388edb81866297ac6df36659f1bf37b
Aligern <sasso.luca93@gmail.com>
2024-03-07 21:08:35 +0100

    added some style

--- script/script.js
+++ script/script.js
@@ -4,41 +4,42 @@ console.log(under18);
 let over65 = 40 / 100;
 console.log(over65);
 
-let elBtngen = document.querySelector('.btn.bg-primary');
+let elBtngen = document.querySelector('.btn.btn-outline-secondary');
 console.log(elBtngen);
 
-let elBtnnull = document.querySelector('.btn.bg-warning');
+let elBtnnull = document.querySelector('.btn.btn-outline-light');
 console.log(elBtnnull);
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
     }
 
+// this button send inputs
 elBtngen.addEventListener('click', function(){
-    // conferma il click su console 
-    console.log('hai confermato');
-    // variabili che agiscono nella funzione
-    let eluserName = document.getElementById('nome');
-    console.log(eluserName);
-    let elAge = document.getElementById('userage');
-    console.log(elAge);
-    let elKmeters = document.getElementById('kilometers').value;
-    console.log(elKmeters);
-    let priceKm = elKmeters * 0.21;
-    console.log(priceKm);
-    let nome = eluserName.value;
-    let eta = elAge.value;
-    let meters = elKmeters.value;
-    console.log(nome, eta, meters);
-    // condizioni 
-    if (eta === 'under18user') {
-        priceKm -= ( priceKm * under18 );
-        document.getElementById('ticket-type').innerHTML = 'Sconto minori';
+// we confirm the click by showing it to the console 
+console.log('hai confermato');
+// function variables
+let eluserName = document.getElementById('nome');
+console.log(eluserName);
+let elAge = document.getElementById('userage');
+console.log(elAge);
+let elKmeters = document.getElementById('kilometers').value;
+console.log(elKmeters);
+let priceKm = elKmeters * 0.21;
+console.log(priceKm);
+let nome = eluserName.value;
+let eta = elAge.value;
+let meters = elKmeters.value;
+console.log(nome, eta, meters);
+// conditions
+ if (eta === 'under18user') {
+    priceKm -= ( priceKm * under18 );
+    document.getElementById('ticket-type').innerHTML = 'Sconto minori';
     } else if (eta === 'over65user') {
         priceKm -= ( priceKm * over65 );
         document.getElementById('ticket-type').innerHTML = 'Sconto over65';
     } else {
-        document.getElementById('ticket-type').innerHTML = 'Biglietto standard';
+            document.getElementById('ticket-type').innerHTML = 'Biglietto standard';
     }
     console.log(`il prezzo è ${priceKm.toFixed(2)}`);
     document.getElementById('ticket-user').innerHTML = `${nome}`;
@@ -46,7 +47,7 @@ elBtngen.addEventListener('click', function(){
     document.getElementById('ticket-id').innerHTML = getRndInteger(80000, 99999);
     document.getElementById('wagon-nmb').innerHTML = getRndInteger(1,9);
 });
-
+// this button nullify the inputs
 elBtnnull.addEventListener('click', function(){
     console.log('hai annullato');
     document.getElementById('nome').value = "";
@@ -55,4 +56,3 @@ elBtnnull.addEventListener('click', function(){
     console.log('cancella tutto')
 }
 )
-
